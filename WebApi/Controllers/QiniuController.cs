96e3b620f4228e53267cbf900c1eea519ad72df9
wawa0210 <xiaozhang0210@hotmail.com>
2018-01-27 19:55:29 +0800

    ueditor 编辑器配置信息ok

---
+++ WebApi/Controllers/QiniuController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Qiniu;
+
+namespace WebApi.Controllers
+{
+    [Route("v0/qinius")]
+    public class QiniuController : BaseApiController
+    {
+        /// <summary>
+        /// 获得qiniutoken信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("token")]
+        public ResponseModel GetQiniuToken()
+        {
+            return Success(new QiniuService().GetToken());
+        }
+
+       
+    }
+}
--- WebApi/Qiniu/QiniuService.cs
+++ WebApi/Qiniu/QiniuService.cs
@@ -13,12 +13,17 @@ namespace WebApi.Qiniu
     {
         public string UploadImg(byte[] imgdata)
         {
+            var saveKey = GuidExtens.GuidTo16String() + ".jpg";
+            var token = GetToken();
+            var formUploader = new FormUploader();
+            var result = formUploader.UploadData(imgdata, saveKey, token);
+            return saveKey;
+        }
+
+        public string GetToken()
+        {
             var mac = new Mac("gXBXI5GVEAZweMD7v9roKmRhcode8fFtnxjd-RkB", "lPqZgqyP3DM8ibLNwq7C5JAzj8Am9_Dhq3vx9IiM");
             var bucket = "qcode";
-            var saveKey = GuidExtens.GuidTo16String() + ".jpg";
-
-            // 上传策略，参见 
-            // https://developer.qiniu.com/kodo/manual/put-policy
             var putPolicy = new PutPolicy();
             // 如果需要设置为"覆盖"上传(如果云端已有同名文件则覆盖)，请使用 SCOPE = "BUCKET:KEY"
             // putPolicy.Scope = bucket + ":" + saveKey;
@@ -31,9 +36,8 @@ namespace WebApi.Qiniu
             // https://developer.qiniu.com/kodo/manual/upload-token            
             var jstr = putPolicy.ToJsonString();
             var token = Auth.CreateUploadToken(mac, jstr);
-            var formUploader = new FormUploader();
-            var result = formUploader.UploadData(imgdata, saveKey, token);
-            return saveKey;
+
+            return token;
         }
     }
 }
