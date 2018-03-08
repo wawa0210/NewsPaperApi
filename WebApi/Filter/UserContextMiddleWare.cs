f82c2addc1f1e6ec3293d4752dfacd97df382b01
wawa0210 <xiaozhang0210@hotmail.com>
2018-03-08 23:37:42 +0800

    添加中间件

---
+++ WebApi/Filter/UserContextMiddleWare.cs
@@ -0,0 +1,32 @@
+﻿using CommonLib.Extensions;
+using EmergencyAccount.Entity;
+using Infrastructure.Context;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Filter
+{
+    public class UserContextMiddleWare
+    {
+        private readonly RequestDelegate _next;
+
+        public UserContextMiddleWare(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var userContext = context.User.Claims.FirstOrDefault().Value;
+            if (!string.IsNullOrEmpty(userContext))
+            {
+                CallContext.SetData("userInfo", userContext.JsonToObj<EntityAccountManager>());
+            }
+
+            await _next(context);
+        }
+    }
+}
